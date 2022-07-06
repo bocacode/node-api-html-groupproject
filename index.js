2d4101feb5732ec6882171a1f9a00052cdd2627d
Stephanie Sadowski <stephaniesadowski@Stephanies-Air.home>
2022-07-06 19:37:37 -0400

    added services folder

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/home/home.html'))
 })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/services/services.html'))
+})
