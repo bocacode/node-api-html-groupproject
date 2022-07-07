8b70658b8784eb4530a9cce87732389d033434a2
Justin <justin@Justins-MacBook-Air.local>
2022-07-06 21:02:23 -0400

    added login feature

--- login-route/index.js
+++ login-route/index.js
@@ -10,6 +10,6 @@ app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, '/login-route/login-route.html'))
 })
